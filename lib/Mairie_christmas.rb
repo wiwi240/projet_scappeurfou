e383723c57ca80e254b0a2fe893fe4b7354856a4
wiwi240 <william.mahi14@gmail.com>
2026-01-29 19:01:50 +0100

    ragequit

--- lib/Mairie_christmas.rb
+++ lib/Mairie_christmas.rb
@@ -1,19 +1,11 @@
-require"nokogiri"
-require"open-uri"
+require "nokogiri"
+require "open-uri"
 
-def get_townhall_email(townhall_url) 
-page = Nokogiri::HTML(URI.open(townhall_url))
+# MÉTHODE 1 : Récupérer l'e-mail d'UNE seule mairie (Ton code déjà testé !)
+def get_townhall_email(townhall_url)
+    page = Nokogiri::HTML(URI.open(townhall_url))
+    mairie = page.xpath('//h1').text
+    email = page.xpath('//*[@id="contentContactEmail"]/span[2]/a').text
 
-# On utilise 'mairie' au lieu de 'liste_mairie' pour correspondre au puts plus bas
-mairie = page.xpath('//h1').text 
-
-# On utilise 'adress_email' au lieu de 'liste_adresse_mail'
-adress_email = page.xpath('//*[@id="contentContactEmail"]/span[2]/a').text
-
-resultat = { mairie => adress_email }
-
-# Maintenant, Ruby va trouver 'mairie' et 'adress_email' sans problème
-puts "la mairie #{mairie} a pour adresse email : #{adress_email}"
-return resultat
-
+    return { mairie => email }
 end
--- spec/Mairie_christmas_spec.rb
+++ spec/Mairie_christmas_spec.rb
@@ -1,20 +1,17 @@
-require_relative '../lib/Mairie_christmas' # On pointe vers ton fichier de code
+require_relative '../lib/mairie_christmas'
 
 describe "La méthode get_townhall_email" do
-# On définit une URL de test (une mairie qui existe vraiment)
-let(:townhall_url) { "https://lannuaire.service-public.gouv.fr/ile-de-france/val-d-oise/mairie-95039-01" }
+  # On teste avec Avernes comme demandé dans l'énoncé
+  let(:townhall_url) { "https://lannuaire.service-public.gouv.fr/ile-de-france/val-d-oise/mairie-95040-01" }
 
-it "devrait retourner un Hash" do
-expect(get_townhall_email(townhall_url)).to be_a(Hash)
+  it "doit retourner un Hash" do
+    expect(get_townhall_email(townhall_url)).to be_a(Hash)
+  end
+
+  it "doit retourner un email valide" do
+    result = get_townhall_email(townhall_url)
+    # Vérifie que la valeur du hash contient un "@"
+    expect(result.values.first).to include("@")
+  end
 end
 
-it "devrait retourner un email valide (contenant un @)" do
-# On récupère la valeur du hash (l'email)
-result = get_townhall_email(townhall_url)
-expect(result.values.first).to include("@")
-end
-
-it "ne devrait pas retourner un résultat vide" do
-expect(get_townhall_email(townhall_url)).not_to be_nil
-end
-end
