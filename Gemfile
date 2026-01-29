1c95d3f104325f67ebca6f1f4274ff22662315bd
wiwi240 <william.mahi14@gmail.com>
2026-01-29 15:36:52 +0100

    maj gemme

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem "rspec"
 gem "pry"
 gem 'nokogiri', '~> 1.15', '>= 1.15.4'
 gem "httparty"
+gem "open-uri"
--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,6 +1,9 @@
 require 'nokogiri'
 require 'httparty'
 
-def perform
-  # Code ici
+def crypto_scrapper
+  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/")) # 1. Ouvrir l'URL
+  nom = page.xpath("//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]/div/a[2]")# 2. Récupérer les noms
+  price = page.xpath("//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[5]/div/span)# 3. Récupérer les prix
+  data = [nom , price]# 4. Associer les deux dans un array de hashs
 end
