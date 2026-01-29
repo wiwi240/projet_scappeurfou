175a87519eeb891a0a080c7798e86eba8d9d5be0
wiwi240 <william.mahi14@gmail.com>
2026-01-29 15:55:50 +0100

    dark_trader code

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,9 +1,50 @@
 require 'nokogiri'
-require 'httparty'
+require 'open-uri'
 
-def crypto_scrapper
-  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/")) # 1. Ouvrir l'URL
-  nom = page.xpath("//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]/div/a[2]")# 2. Récupérer les noms
-  price = page.xpath("//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[5]/div/span)# 3. Récupérer les prix
-  data = [nom , price]# 4. Associer les deux dans un array de hashs
+def get_crypto_data
+  # 1. DEFINITION DE LA SOURCE
+  # On stocke l'adresse du site dans une variable pour plus de clarté.
+  url = "https://coinmarketcap.com/all/views/all/"
+  
+  # 2. CAPTURE DU HTML
+  # URI.open(url) télécharge la page.
+  # Nokogiri::HTML(...) transforme ce téléchargement en un objet structuré (DOM) que Ruby peut fouiller.
+  page = Nokogiri::HTML(URI.open(url))
+
+  # 3. EXTRACTION DES DONNEES BRUTES (NodeSets)
+  # .xpath cherche dans l'objet 'page' tous les éléments correspondant au chemin.
+  # On récupère ici des collections d'objets HTML (pas encore du texte pur).
+  symbols_list = page.xpath('//td[contains(@class, "cmc-table__cell--sort-by__symbol")]')
+  prices_list = page.xpath('//td[contains(@class, "cmc-table__cell--sort-by__price")]')
+
+  # 4. INITIALISATION DU CONTENEUR FINAL
+  # On crée un tableau vide qui accueillera nos futurs Hashs.
+  crypto_array = []
+
+  # 5. BOUCLE DE TRAITEMENT ET D'ASSOCIATION
+  # .each_with_index parcourt la liste des symboles et nous donne 'index' (0, 1, 2...) pour chaque tour.
+  symbols_list.each_with_index do |symbol_node, index|
+    
+    # a. Nettoyage du prix :
+    # .text extrait la String (ex: "$5,245.12") de l'objet HTML.
+    # .delete enlève les caractères gênants.
+    # .to_f convertit le texte restant en nombre décimal (Float).
+    clean_price = prices_list[index].text.delete("$").delete(",").to_f
+    
+    # b. Création du Hash :
+    # On crée une paire { "NOM" => PRIX }
+    crypto_hash = { symbol_node.text => clean_price }
+    
+    # c. Stockage :
+    # L'opérateur << ajoute le petit hash dans le grand tableau.
+    crypto_array << crypto_hash
+    
+    # d. Feedback (Effet Hacker) :
+    # Affiche l'avancement dans le terminal pendant l'exécution.
+    puts "#{symbol_node.text} => #{clean_price}"
+  end
+
+  # 6. RETOUR DE LA METHODE
+  # En Ruby, la dernière ligne évaluée est renvoyée par la méthode.
+  crypto_array
 end
---
+++ spec/dark_trader_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/dark_trader'
+
+describe "the get_crypto_data method" do
+it "returns an array of hashes" do
+    expect(get_crypto_data).to be_an_instance_of(Array)
+    expect(get_crypto_data.all? { |i| i.is_a?(Hash) }).to be true
+end
+
+it "contains specific cryptocurrencies" do
+    result = get_crypto_data
+        # Vérification de la présence de clés symboles courantes
+    symbols = result.map(&:keys).flatten
+    expect(symbols).to include("BTC")
+    expect(symbols).to include("ETH")
+end
+
+it "is not empty and has a minimum size" do
+    expect(get_crypto_data.length).to be > 100
+end
+end
